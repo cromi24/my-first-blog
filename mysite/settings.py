1900c92bff19cd1e050627e6be66e50dcd2dd00e
cromi24 <cromi24@yandex.ru>
2022-06-04 18:19:13 +0300

    some css changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+########DELETE IN PRODUCTION
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
